a6068dddd00483f000519b7d695cf519b6ece7d7
SANKETJAIN1111 <sigmasanket2712@gmail.com>
2022-02-10 04:13:20 +0530

    first Project

---
+++ spring-boot-auth-client/src/main/java/com/legatohealth/controller/PermissionGranter.java
@@ -0,0 +1,24 @@
+package com.legatohealth.controller;
+
+import javax.activation.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class PermissionGranter extends ResourceServerConfigurerAdapter{
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+	
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/protected-app/**").hasAnyRole("ADMIN","USER")
+		.antMatchers(HttpMethod.POST,"/protected-app/**").hasAnyRole("ADMIN");
+		// TODO Auto-generated method stub
+	}
+
+	
+	
+}
